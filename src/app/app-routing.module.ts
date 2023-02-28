5afc11837f67b11ee15d0d0b304cf38acc85d8d2
IfraHernandez <ifrahim.hernandez@assuredtechmatics.com>
2023-02-28 17:22:31 -0500

    Template upgrade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthLayoutComponent } from "./layout/auth-layout/auth-layout.component";
+import { AppLayoutComponent } from "./layout/app-layout/app-layout-component";
+
+import { AUTH_LAYOUT_ROUTES } from "./routes/auth-layout.routes";
+import { APP_LAYOUT_ROUTES } from './routes/app-layout.routes';
+
+const appRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full',
+    },
+    { 
+        path: '', 
+        component: AppLayoutComponent,
+        children: APP_LAYOUT_ROUTES 
+    },
+    { 
+        path: '', 
+        component: AuthLayoutComponent, 
+        children: AUTH_LAYOUT_ROUTES
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(appRoutes, {
+            preloadingStrategy: PreloadAllModules,
+            anchorScrolling: 'enabled',
+            scrollPositionRestoration: 'enabled',
+            relativeLinkResolution: 'legacy'
+        })
+    ],
+    exports: [
+        RouterModule
+    ]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
